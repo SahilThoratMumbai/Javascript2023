6bf679b7ea02e54299d9c0679620e368284b093a
SahilSomnathThorat <125123211+SahilThoratMumbai@users.noreply.github.com>
2023-08-29 13:05:10 +0000

    datatypes-summary

--- .devcontainer/01_basics/03_conversionOperation.js
+++ .devcontainer/01_basics/03_conversionOperation.js
@@ -1,11 +1,50 @@
-console.log("Hello");
+// console.log("Hello");
 
-let score=33
-console.log(typeof score);
+// let score=33
+// console.log(typeof score);
 
-let loggedIn=1
-let isLoggedIn=Boolean(loggedIn)
-console.log(isLoggedIn);
+// let loggedIn=1
+// let isLoggedIn=Boolean(loggedIn)
+// console.log(isLoggedIn);
 
 //true=>1  false=>0
 //""=> false   "sahil"=>True
+
+
+/*********OPERATIONS*********/
+
+// let value=3
+// let negValue=-value
+// console.log(negValue);
+
+// console.log(2+3);
+// console.log(2-3);
+// console.log(2*3);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+// let firstName="Sahil"
+// let middleName=" Somnath"
+// let lastName=" Thorat"
+
+// let name=firstName+middleName+lastName
+// console.log(name);
+
+// console.log(2+3);
+// console.log(2+"3");
+// console.log(2+2+"5");
+
+// let gameCounter=100
+// ++gameCounter
+// console.log(gameCounter);
+
+let x=3
+let y=x++
+console.log(x,y);
+//output is 4,3
+
+let a=3
+let b=++x
+console.log(a,b);
+//output is 4,4
---
+++ .devcontainer/01_basics/datatypes-summary.js
@@ -0,0 +1,28 @@
+//Primitive
+
+//7 Types String,Number,Boolean,null,undefined,symbol,BigInt
+
+let name="Sahil"
+const roll=70
+let isLoggedIn=true
+
+let division;
+const bigNumber=324234242973234232323424n  //BigInt
+
+
+//Non Primitive(Reference)
+//Array,Objects,Functions
+const heroes=["Bhise","Unde","Tawade"]
+let myObj={
+    Name:"Sahil",
+    Age:20,
+    Profession:"Student",
+}
+
+//Functions 
+const myfunction=function(){
+    console.log("Hello World");
+}
+
+console.log(typeof myObj);
+//JS is dynamicallt typed programming language
