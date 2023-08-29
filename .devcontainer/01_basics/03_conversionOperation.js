9e2b09dc8d20deb4da8031d00666a818c3daabf8
SahilSomnathThorat <125123211+SahilThoratMumbai@users.noreply.github.com>
2023-08-29 11:41:01 +0000

    coversion

---
+++ .devcontainer/01_basics/03_conversionOperation.js
@@ -0,0 +1,11 @@
+console.log("Hello");
+
+let score=33
+console.log(typeof score);
+
+let loggedIn=1
+let isLoggedIn=Boolean(loggedIn)
+console.log(isLoggedIn);
+
+//true=>1  false=>0
+//""=> false   "sahil"=>True
