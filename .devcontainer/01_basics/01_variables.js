29a055c05b011c6985bd1a7484ea4fc86aebee8f
SahilSomnathThorat <125123211+SahilThoratMumbai@users.noreply.github.com>
2023-08-29 07:33:39 +0000

    commit

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,9 @@
+const accountID=7887
+let accountEmail="xyz@gmail.com"
+var accountPassword="1234"
+/*
+ Not to use var 
+ because of block scope and functional scope
+*/
+accountCity="Kolkata"
+console.table([accountID,accountEmail,accountPassword,accountCity])
